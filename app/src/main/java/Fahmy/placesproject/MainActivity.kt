99166fc823aa356060b4039c946f2b88bc337e59
fahmy <mohamedabdelnabyfahmy@gmail.com>
2020-03-16 20:56:05 +0200

    initial project

---
+++ app/src/main/java/Fahmy/placesproject/MainActivity.kt
@@ -0,0 +1,21 @@
+package Fahmy.placesproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import fahmy.smartplaces.features.home.SmartPlaces
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+
+            SmartPlaces.start(this, supportFragmentManager) {
+                println(it)
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/Fahmy/placesproject/MainApp.kt
@@ -0,0 +1,16 @@
+package Fahmy.placesproject
+
+import android.app.Application
+import fahmy.smartplaces.features.home.SmartPlaces
+
+//
+// Created by fahmy on 3/16/20.
+//
+
+class MainApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        SmartPlaces.Initialize(getString(R.string.apiKey), this)
+    }
+}
